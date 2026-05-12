d27edc623f79fe555598d83e8c9d716744907c1e
SantiagoPena2804 <davidsa.pena@urosario.edu.co>
2026-05-11 19:52:00 -0500

    Upload parte Peña

--- DisenoArquitecturaClase (1)/pranalab/src/routes/panel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { Router } = require('express');
-const router = Router();
-const { getPanelDT } = require('../controllers/PanelController');
-
-// GET /panel — Dashboard del Director Técnico
-router.get('/', getPanelDT);
-
-module.exports = router;
